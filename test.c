450e0dd2988c9ecddf31b447ad50a7019684c7c0
cy21264 <cy21264@shibaura-it.ac.jp>
2023-10-05 14:17:05 +0900

    change test.c ok?

--- test.c
+++ test.c
@@ -1,10 +1,5 @@
 #include <fcntl.h>
 #include <unistd.h>
 int main() {
-	char buff[10];
-	int fd;
-	fd = open("test.txt", O_RDONLY);
-	read(fd, buff, 10);
-	write(1, buff, 10); 
-	close(fd); 
+	printf("hello,World!");
 }
